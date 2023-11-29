011192cb6aa22fdf81df578bd38f4488e38cb0bf
David <josedago1163@gmail.com>
2023-11-29 17:02:30 -0500

    empezamos la galeria

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import Principal from './pages/Principal';
+import Galeria from './pages/Galeria';
+import Contact from './pages/Contact';
+import Servicios from './pages/Servicios';
 import './App.css';
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 
@@ -9,8 +11,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        
         <Route path="/" element={<Principal />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/servicios" element={<Servicios />} />
       </Routes>
       </BrowserRouter>
     </div>
