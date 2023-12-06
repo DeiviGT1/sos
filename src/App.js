8b8e69706145038fcf57196527d02e1f815ec18c
David <josedago1163@gmail.com>
2023-12-06 08:44:28 -0500

    empezamos la version sin usuarios

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import Principal from './pages/Principal';
 import Galeria from './pages/Galeria';
 import Contact from './pages/Contact';
 import Servicios from './pages/Servicios';
-import Profile from './pages/Profile';
-import LoginPage from './pages/LogMod/LoginPage';
-import Register from './pages/LogMod/Register';
-import LogoutPage from './pages/LogMod/LogoutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
 import { Route, Routes, HashRouter } from "react-router-dom"
-import { AuthProvider, AuthRoute } from "./pages/LogMod/auth"
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
-        <AuthProvider>
+
           <Header />
             <Routes>
             
@@ -26,27 +21,10 @@ function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/servicios" element={<Servicios />} />
               
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<Register />} />
-              <Route 
-                path="/profile" 
-                element={
-                  <AuthRoute>
-                    <Profile />
-                  </AuthRoute>
-                } 
-                />
-              <Route 
-                path="/logout" 
-                element={
-                  <AuthRoute>
-                    <LogoutPage />
-                  </AuthRoute>
-                  } />
             
             </Routes>
           <Footer />
-        </AuthProvider>
+
       </HashRouter>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,14 @@ import './Header.module.css';
 import logo from '../../src/assets/images/color-no-fondo.png'
 import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useAuth } from '../pages/LogMod/auth';
 
 const routes = [];
 routes.push({ path: "/", name: "Home", private: false });
 routes.push({ path: "/galeria", name: "Galeria", private: false });
 routes.push({ path: "/contact", name: "Contact", private: false });
 
-const routesLog = [];
-routesLog.push({ path: "/login", name: "Login", private: false, onlyPublic: true});
-routesLog.push({ path: "/register", name: "Register", private: false, onlyPublic: true});
-routesLog.push({ path: "/profile", name: "Profile", private: true, });
-routesLog.push({ path: "/logout", name: "Logout", private: true, });
 
 function Header() {
-  const auth = useAuth();
 
   useEffect(() => {
     let prevScrollPos = window.pageYOffset;
@@ -69,19 +62,7 @@ function Header() {
             </li>
             ))}
         </ul>
-        <ul className="log">
-        {routesLog.map(routeLog => (
-          (routeLog.private && !auth.user) || (routeLog.onlyPublic && auth.user) ? null : (
-            <li key={routeLog.path}>
-              <NavLink to={routeLog.path} className="active">
-                <span style={{ textTransform: 'uppercase' }}>{routeLog.name}</span>
-              </NavLink>
-            </li>
-            )
-        ))}
 
-
-        </ul>
       </nav>
     </header>
   );
