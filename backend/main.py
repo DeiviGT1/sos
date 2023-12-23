89af5bcf1c881c5cf375122249c3222d949cb994
David <josedago1163@gmail.com>
2023-12-23 17:58:31 -0500

    version todo en 0.0.0.0

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = FastAPI()
 
 # Enable CORS
-origins = ["http://127.0.0.1:3000"]
+origins = ["http://0.0.0.0:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -75,9 +75,6 @@ def enviar_formulario(data: FormData):
     error_response = JSONResponse(content={"status": "Error", "detail": str(e)}, status_code=422)
 
     return error_response
-
-
-print(f"Server is running on http://127.0.0.1:3001")
 
 if __name__ == '__main__':
   uvicorn.run("main:app", host='0.0.0.0', port=3001)
